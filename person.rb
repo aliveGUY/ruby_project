550408fbdec1132b3dfa221f2446219f2e840fe8
aliveGUY <ilyadubrivvin@gmail.com>
2022-07-21 03:28:05 +0300

    fix linter errors

--- person.rb
+++ person.rb
@@ -2,6 +2,7 @@ require './nameable'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -13,11 +14,10 @@ class Person < Nameable
   def can_use_services?
     of_age? || parent_permission
   end
-  
+
   private
 
   def of_age?
     age >= 18
   end
-
 end
