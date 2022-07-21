cad0ed14372c381b5b37802ab665d65cf8f61aba
aliveGUY <ilyadubrivvin@gmail.com>
2022-07-21 03:19:59 +0300

    Implemet_decorate_pattern

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
@@ -11,10 +13,11 @@ class Person
   def can_use_services?
     of_age? || parent_permission
   end
-
+  
   private
 
   def of_age?
     age >= 18
   end
+
 end
