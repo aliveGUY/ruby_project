03e0f3605de561e1f9db6a603b57d96a38e5a3b9
wandergithub <wandergonzalez0@hotmail.com>
2022-07-28 14:41:23 -0400

    Add Teacher class specs

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../teacher'
+
+describe  Teacher do
+
+  before :each do
+    allow_any_instance_of(Teacher).to receive(:gets).and_return "12", "name", "Doctor"
+    @teacher = Teacher.new([])
+  end
+  
+  describe "Class initialization" do
+    it "Return a Teacher object instance" do
+      expect(@teacher).to be_instance_of Teacher
+    end
+
+    it "Has atributes: specialization, id, age, name, parent_permission, rentail" do
+      expect(@teacher.id).to be_truthy
+    end
+  end
+  
+  describe "#can_use_services?" do
+    it "returns true" do
+      expect(@teacher.can_use_services?).to be true
+    end
+  end
+end
