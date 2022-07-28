5d455d194f35fc12e690594a74119b7822d37bb2
wandergithub <wandergonzalez0@hotmail.com>
2022-07-28 16:24:20 -0400

    Create Rentail class tests

---
+++ spec/rentail_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../rentail'
+
+describe Rentail do
+  before :each do
+    allow_any_instance_of(Rentail).to receive(:gets).and_return '0', '0'
+  end
+
+  describe "Class initialize" do
+    it "is initialized with expected methods" do
+      student1 = double('student') 
+      person1 = double('person') 
+      
+      allow(student1).to receive(:save_rental)
+      allow(person1).to receive(:save_rental) { 'Jill Smith'} 
+
+      rental = Rentail.new(["something"], ["something"], [student1], [person1])
+      expect(rental).to be_truthy
+    end
+  end
+
+end
